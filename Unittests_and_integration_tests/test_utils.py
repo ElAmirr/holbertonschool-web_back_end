9d692c56e9d1c628723c9f64fcf2ee411ce3e819
ElAmirr <othmaniamir41@gmail.com>
2024-07-08 12:35:45 +0100

    push

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -1,30 +1,15 @@
 import unittest
-from unittest.mock import patch
-from utils import memoize  # Make sure to import the actual memoize decorator
+from parameterized import parameterized
+from utils import access_nested_map  # Make sure to import the actual function
 
-class TestMemoize(unittest.TestCase):
-    def test_memoize(self):
-        class TestClass:
-            def a_method(self):
-                return 42
-
-            @memoize
-            def a_property(self):
-                return self.a_method()
-
-        with patch.object(TestClass, 'a_method', return_value=42) as mock_method:
-            test_instance = TestClass()
-            
-            # Call a_property twice
-            result1 = test_instance.a_property
-            result2 = test_instance.a_property
-            
-            # Check that the method was called only once
-            mock_method.assert_called_once()
-            
-            # Check that the returned results are correct
-            self.assertEqual(result1, 42)
-            self.assertEqual(result2, 42)
+class TestAccessNestedMap(unittest.TestCase):
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2),
+    ])
+    def test_access_nested_map(self, nested_map, path, expected):
+        self.assertEqual(access_nested_map(nested_map, path), expected)
 
 if __name__ == '__main__':
     unittest.main()
