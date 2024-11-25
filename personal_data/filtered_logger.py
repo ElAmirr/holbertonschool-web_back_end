158e3544a408f6075a6911bace25ac558a6d7c2f
ElAmirr <othmaniamir41@gmail.com>
2024-11-25 08:33:33 +0100

    task 1

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -1,4 +1,21 @@
-fields = ["password", "date_of_birth"]
-message = "name=egg;email=eggmin@eggsample.com;password=eggcellent;date_of_birth=12/12/1986;"
-print(filter_datum(fields, "xxx", message, ";"))
-# Output: name=egg;email=eggmin@eggsample.com;password=xxx;date_of_birth=xxx;
+#!/usr/bin/env python3
+""" Filtered Logger Module """
+import re
+from typing import List
+
+
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+    """
+    Obfuscates specified fields in a log message.
+
+    Args:
+        fields: A list of strings representing fields to obfuscate.
+        redaction: The string to replace field values with.
+        message: The log line to be obfuscated.
+        separator: The character separating fields in the log line.
+
+    Returns:
+        A string with specified fields obfuscated.
+    """
+    pattern = f"({'|'.join(fields)})=[^{separator}]*"
+    return re.sub(pattern, lambda m: f"{m.group(1)}={redaction}", message)
