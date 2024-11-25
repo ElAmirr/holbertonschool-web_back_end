b1475672ef8a168a7909b196e5bf668074057889
ElAmirr <othmaniamir41@gmail.com>
2024-11-25 08:43:57 +0100

    task 1

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """
-Module for obfuscating log messages.
+Module for filtering log messages.
 
-This module provides the `filter_datum` function for obfuscating sensitive
-information in log messages using regex.
+This module provides a function to obfuscate sensitive information
+in log messages based on given fields.
 """
 
 import re
@@ -18,10 +18,10 @@ def filter_datum(fields: List[str], redaction: str, message: str, separator: str
         fields (List[str]): A list of strings representing fields to obfuscate.
         redaction (str): The string to replace the field values with.
         message (str): The log message to be obfuscated.
-        separator (str): The character separating the fields in the log message.
+        separator (str): The character separating fields in the log message.
 
     Returns:
         str: The obfuscated log message.
     """
-    pattern = '|'.join([f"{field}=[^\\{separator}]*" for field in fields])
-    return re.sub(pattern, lambda match: f"{match.group().split('=')[0]}={redaction}", message)
+    pattern = f"({'|'.join(fields)})=[^\\{separator}]*"
+    return re.sub(pattern, lambda m: f"{m.group(1)}={redaction}", message)
