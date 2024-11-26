9a00ac68e6ba4cd46049c3a51716134e67824863
ElAmirr <othmaniamir41@gmail.com>
2024-11-26 22:50:52 +0100

    push

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -6,15 +6,12 @@ app = Flask(__name__)
 
 AUTH = Auth()
 
+
 @app.route("/sessions", methods=["POST"])
 def login():
     """Handles the login functionality."""
     email = request.form.get("email")
     password = request.form.get("password")
-
-    # Check if the email and password are provided
-    if not email or not password:
-        abort(400, description="Missing email or password")
 
     # Check if credentials are valid
     if not AUTH.valid_login(email, password):
@@ -30,6 +27,7 @@ def login():
     response.set_cookie("session_id", session_id)
 
     return response
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
