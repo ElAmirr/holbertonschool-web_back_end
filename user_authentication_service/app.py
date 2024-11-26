15583076bdc872bfd9be7b56afe2beafa69bf1e6
ElAmirr <othmaniamir41@gmail.com>
2024-11-26 22:34:48 +0100

    push

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -4,6 +4,7 @@ Basic Flask app
 """
 from flask import Flask, jsonify
 
+# Create a Flask instance
 app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
@@ -13,6 +14,6 @@ def home():
     """
     return jsonify({"message": "Bienvenue"})
 
-
+# Start the app if this module is executed directly
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
