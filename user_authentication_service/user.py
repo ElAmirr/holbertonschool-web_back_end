cc11fd52b5c572efe04c58ad6e363fb57cbf2259
ElAmirr <othmaniamir41@gmail.com>
2024-11-26 22:24:06 +0100

    push

--- user_authentication_service/user.py
+++ user_authentication_service/user.py
@@ -1,11 +1,19 @@
+#!/usr/bin/env python3
+"""
+User model using SQLAlchemy
+"""
+
 from sqlalchemy import Column, Integer, String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
 class User(Base):
-    """User model for the users table"""
+    """
+    User model for the `users` table
+    """
     __tablename__ = 'users'
+
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
